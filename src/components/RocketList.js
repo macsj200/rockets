3e297c7cb3918c7e17fbdd74c92cf433dcddd8f1
macsj200 <maxjohansen@berkeley.edu>
2018-08-19 16:28:40 -0600

    holder

---
+++ src/components/RocketList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { RocketCard } from './RocketCard';
+
+export class RocketList extends Component {
+    render() {
+        const { rockets } = this.props;
+        return (
+            <div className="row">
+                {
+                rockets.map(rocket => <RocketCard key={rocket._id} rocket={rocket} />)
+                }
+            </div>
+        );
+    }
+}
