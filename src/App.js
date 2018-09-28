1b7e59b7485515ec1b7b9c54006b2d898a5f23c3
macsj200 <maxjohansen@berkeley.edu>
2018-09-28 00:02:52 -0600

    wip

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
-import saturnv from './saturnv.jpg';
+import atlasv from './atlasv.jpg';
 import falconheavy from './falconheavy.jpg';
 import { RocketList } from './components/RocketList';
 
 export const RocketContext = React.createContext('rockets');
+
+function DisplayDuck(props) {
+  return <h3>{props.duck.name}</h3>
+}
+
+function ListDucks(props) {
+  return props.ducks.map(duck => <DisplayDuck duck={duck} />)
+}
+
+function DuckScreen(props) {
+  return <ListDucks ducks={props.ducks} />
+}
+
+function AppContainer(props) {
+  return <DuckScreen ducks={props.ducks} />
+}
 
 class App extends Component {
   state = {
@@ -15,8 +31,8 @@ class App extends Component {
         _id: 0
       },
       {
-        title: "Saturn V",
-        images: [saturnv],
+        title: "Atlas V",
+        images: [atlasv],
         _id: 1
       }
     ]
@@ -26,6 +42,7 @@ class App extends Component {
     return (
       <RocketContext.Provider value={rockets}>
         <div className="container">
+          <DuckScreen ducks={[ {name: 'Donald' }, { name: 'Scrooge McDuck' }]} />
           <div className="row">
             <div className="col-sm">
               <h1 className="text-center">Rockets</h1>
